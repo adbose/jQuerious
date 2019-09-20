4ab313923badcfaeed3d816742cef1817b83d2e9
Aditya Bose <13054902+adbose@users.noreply.github.com>
2019-09-20 15:05:05 +0530

    Updated code to make get request

--- js/jq.js
+++ js/jq.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-     $("#btnSubmit").on('click', function (event) {
-           event.preventDefault();
-           var el = $(this);
-           el.prop('disabled', true);
-           window.open("https://www.github.com");
-           setTimeout(function(){el.prop('disabled', false); }, 3000);
-     });
+    $("#btnSubmit").on('click', function (event) {
+        event.preventDefault();
+        var el = $(this);
+        el.prop('disabled', true);
+        //window.open("https://www.github.com");
+        $.get('https://api.github.com/users/adbose', function( data ) {
+            alert( "Data Loaded!\nUsername is: " + data['login'] );
+            setTimeout(function(){el.prop('disabled', false); }, 1000);
+        });
+    });
+           //setTimeout(function(){el.prop('disabled', false); }, 3000);
 });
